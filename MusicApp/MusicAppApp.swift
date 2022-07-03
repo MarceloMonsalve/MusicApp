9cb461255101795b1bed78430f8cc4c34ff1403e
Marcelo Monsalve <mm.hakanson@gmail.com>
2022-07-03 16:39:37 -0700

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Marcelo Monsalve on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
